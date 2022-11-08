0c13d2d305655b273ef192e49e048174d5f1ddef
Eric Myhre <hash@exultant.us>
2022-11-08 17:24:33 +0100

    basic functioning of shell exec feature.
    Going incrementally on the error handling part.
    Won't be surprised if that requires more iterations,
    especially in the face of platform variation.
    The error code strings themselves are also first-pass placeholders.
    
    Works, though.

--- pkg/action/cmd.go
+++ pkg/action/cmd.go
@@ -2,10 +2,12 @@ package action
 
 import (
 	"fmt"
+	"os/exec"
+	"strconv"
+	"syscall"
 
 	"github.com/serum-errors/go-serum"
 	"go.starlark.net/starlark"
-	// "github.com/warpsys/wfx/pkg/wfx"
 )
 
 var _ starlark.Callable = (*CmdAction)(nil)
@@ -26,10 +28,13 @@ func (a *CmdAction) CallInternal(thread *starlark.Thread, args starlark.Tuple, k
 		if len(kwargs) < 1 {
 			return starlark.None, a.errInvalidArgs("no args received")
 		}
-		// TODO
+		panic("wfx: not yet implemented kwargs for cmd action")
 	case 1:
 		// Exec time.
-		thread.Print(thread, fmt.Sprintf("cmd: would invoke: %q\n", args[0]))
+		incantation := string(args[0].(starlark.String))
+		thread.Print(thread, fmt.Sprintf("cmd: would invoke: %q\n", incantation))
+		cmd := exec.Command(a.interpreter, "-c", incantation)
+		return starlark.None, a.processExecError(cmd.Run(), incantation)
 	default:
 		return starlark.None, a.errInvalidArgs("received too many args")
 	}
@@ -51,5 +56,33 @@ func (a *CmdAction) init() {
 }
 
 func (CmdAction) errInvalidArgs(reason string) error {
-	return serum.Errorf("wfx-error-invalid-args", "the cmd action needs either one string arg, or kwargs: "+reason) // FIXME: tweak packages until I can use a constant here without an import cycle problem.
+	return serum.Errorf("wfx-script-error-invalid-args", "the cmd action needs either one string arg, or kwargs: "+reason) // FIXME: tweak packages until I can use a constant here without an import cycle problem.
+}
+
+func (CmdAction) processExecError(original error, incantation string) error {
+	switch e2 := original.(type) {
+	case nil:
+		return nil
+	case *exec.ExitError:
+		if e2.Exited() { // true means code; false means signal
+			code := e2.ExitCode() // I don't think this exists on windows.  Ignoring for now; platform support can be "future work".
+			return serum.Error("wfx-script-aborted-cmd-unhappy",
+				serum.WithMessageTemplate("cmd {{cmd|q}} exited with code {{exitcode}}"),
+				serum.WithDetail("cmd", incantation),
+				serum.WithDetail("exitcode", strconv.Itoa(code)),
+				serum.WithCause(original),
+			)
+		} else {
+			signal := int(e2.Sys().(syscall.WaitStatus).Signal())
+			return serum.Error("wfx-script-aborted-cmd-unhappy",
+				serum.WithMessageTemplate("cmd {{cmd}} exited due to signal {{signal}}"), // Future: wish there was a "quote" fmt directive we could use here.
+				serum.WithDetail("cmd", incantation),
+				serum.WithDetail("signal", strconv.Itoa(signal)),
+				serum.WithCause(original),
+			)
+		}
+		// fun fact: you can report `e2.SystemTime()` and `e2.UserTime()`, too.  Might be worth making this loggable.
+	default:
+		panic(fmt.Errorf("wfx: unknown error from process exec library: %T %w", original, original))
+	}
 }
