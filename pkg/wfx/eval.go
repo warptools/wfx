099773c17ca4a2b74fc0045cf5292ec3e65241dc
Eric Myhre <hash@exultant.us>
2022-11-14 23:35:08 +0100

    drop other experimental bits.  they may return in the future (probably generalized), but aren't useful for now.

--- pkg/wfx/eval.go
+++ pkg/wfx/eval.go
@@ -31,42 +31,6 @@ func (x *FxFile) FirstPass(output io.Writer) (starlark.StringDict, error) {
 		return nil, err
 	}
 
-	// This walk finds a few magic functions and if their arguments are themselves func calls, it rewrites them to have more parameters.
-	// Isn't that neat?
-	// This bit might be a bit of a prototype and may disappear again.
-	syntax.Walk(x.ast, func(n syntax.Node) bool {
-		switch n := n.(type) {
-		case *syntax.CallExpr:
-			if ident, ok := n.Fn.(*syntax.Ident); ok {
-				if ident.Binding.(*resolve.Binding).Scope == 0x5 && ident.Name == "pipe" {
-					//fmt.Printf("::magic engaged\n")
-					for _, arg := range n.Args {
-						if call, ok := arg.(*syntax.CallExpr); ok {
-							call.Args = append(call.Args, &syntax.BinaryExpr{
-								Op: syntax.EQ,
-								X:  &syntax.Ident{Name: "_pipe"},
-								Y:  &syntax.Literal{Token: syntax.STRING, Value: "please"},
-							})
-						}
-					}
-				}
-				// fmt.Printf(":: %#v --- %#v\n", n.Fn, ident.Binding)
-				// so we can dive up through `ident.Binding`... though it's by index, into other tables, so it's a little complicated.
-				// and i think that table... you have to hold onto the nearest enclosing DefStmt or File in order to have access to it.  which this walk callback doesn't really have an easy way to do.
-				// (also the docs a bit wrong; you have to jump `.(*syntax.DefStmt).Function.(*resolve.Function).Locals` (or similar).)
-				// You need to do all that jumping to get ... anything useful really?
-				// The `*resolve.Binding` struct as a `.First` member which might be worth using, but probably doesn't remove most of the other fiddling here.
-				//   wait what haha... actually this is the _only_ place you get an offramp back to `syntax.Ident`, lol?!
-				//   but it's absent for globals or predeclared??  goodness gracious.  so in that case: you have to remember the name you saw earlier, and use that if e.g. resolve.Binding.Scope==0x5.  fun.
-				// I think the `*resolve.Function` object probably is the root truth for unique defn, once you get there.
-				//   But also it's really only got a name string and syntax position.  (and pointers back again to `Params []syntax.Expr` and `Body []syntax.Stmt`.)
-				//     So that kind limits the easily accessable options if we wanted to let someone declare their own functions to be controllers and be able to detect that easily, so, take note.
-				// Okay.  Uh.  Overall: this is quite a maze.  Maybe we should just start with only supporting this feature on globals.  Also if you rename them it'll fail.  Sorry.  Future work!
-			}
-		}
-		return true
-	})
-
 	// This walk finds any statements which contain just a call expression, and rewrites them so they're wrapped in a certain magic function.
 	// We use this to make some very fun DSL.
 	syntax.Walk(x.ast, func(n syntax.Node) bool {
@@ -88,14 +52,6 @@ func (x *FxFile) FirstPass(output io.Writer) (starlark.StringDict, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	// syntax.Walk(x.ast, func(n syntax.Node) bool {
-	// 	switch n := n.(type) {
-	// 	case *syntax.CallExpr:
-	// 		fmt.Printf(":::: callExpr now marked at: %#v\n", n) // you can eyeball the syntax position markers in this.  They don't move, despite all our fuckery above!  Neat!
-	// 	}
-	// 	return true
-	// })
 
 	thread := &starlark.Thread{
 		Name: "exploration",
